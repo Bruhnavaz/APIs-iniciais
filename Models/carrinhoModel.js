2e236433a3b9315208fdc5412b71672622446545
evertonYamata <evertonbssantos1204@gmail.com>
2023-04-26 17:30:23 -0300

    itens do carrinho

--- Models/carrinhoModel.js
+++ Models/carrinhoModel.js
@@ -2,19 +2,27 @@ import { Sequelize } from "sequelize"
 import db from "./db.js"
 
 const Carrinho = db.define("carrinho",{
-    //Arrumar
-        id: {
+    //Arrumar(forma de colocar cada item de forma individual e suas respectivas quantodades)
+        idCarrinho: {
             type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true
         },
-        name: {
+        valor: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        itens: {
             type: Sequelize.STRING,
             allowNull: false
         },
-        email: {
-            type: Sequelize.STRING,
+        quantidade: {
+            type: Sequelize.INTEGER,
             allowNull: false
+        },
+        endereco: {
+            type: Sequelize.STRING,
+            allowNull: true
         }
     
     })
