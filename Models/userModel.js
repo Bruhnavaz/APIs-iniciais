425d3d95fcf70b083fb42c2325310c9ba893f6bd
Bruna Vaz <vazbruna203@gmail.com>
2023-05-10 09:53:47 -0300

    Atualizacao Rotas API

--- Models/userModel.js
+++ Models/userModel.js
@@ -8,23 +8,39 @@ const User = db.define("user",{
         autoIncrement: true,
         primaryKey: true
     },
-    name: {
+    cpf: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    nome: {
         type: Sequelize.STRING,
         allowNull: false
     },
     email: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    rua: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numero: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    bairro: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cidade: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    estado: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 
 })
-
-
-
-
-
-
-
-
 
 export default User
